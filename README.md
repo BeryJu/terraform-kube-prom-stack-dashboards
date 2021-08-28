dc27d1d392de266aef033174b582bf0d7c74dfbb
Jens Langhammer <jens.langhammer@beryju.org>
2021-08-28 13:34:28 +0200

    build tf module

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Normally, the dashboards for the kube-stack are only distributed as ConfigMaps. This repo contains the dashboards as regular JSON files.
 
 Dashboards are built automatically once a day.
+
+Additionally, there's a terraform module in `module/`
--- main.py
+++ main.py
@@ -3,22 +3,44 @@ from requests import get
 from yaml import load, FullLoader
 from os import makedirs
 
-output = "dashboards/"
+raw_output = "dashboards/"
+tf_output = "module"
+
 upstream_sources = [
     "https://raw.githubusercontent.com/prometheus-operator/kube-prometheus/main/manifests/grafana-dashboardDefinitions.yaml"
 ]
 
-makedirs(output, exist_ok=True)
+makedirs(raw_output, exist_ok=True)
+makedirs(tf_output, exist_ok=True)
 
 def build(doc: dict):
     for config_map in doc.get("items", []):
         for file, dashboard in config_map.get("data", {}).items():
             print(f"- writing {file}")
-            with open(f"{output}/{file}", "w+", encoding="utf-8") as dbf:
+            with open(f"{raw_output}/{file}", "w+", encoding="utf-8") as dbf:
                 dbf.write(dashboard)
+
+def build_tf(doc: dict):
+    main_tf = ""
+    for config_map in doc.get("items", []):
+        for file, _ in config_map.get("data", {}).items():
+            slug = file.replace(".json", "")
+            main_tf += f"""
+data "http" "k8s-{slug}" {{
+  url = "https://raw.githubusercontent.com/BeryJu/kube-stack-dashboards/master/dasbhoards/{file}"
+}}
+
+resource "grafana_dashboard" "k8s-{slug}" {{
+  config_json = data.http.k8s-{slug}.body
+}}
+"""
+    with open(f"{tf_output}/main.tf", "w+", encoding="utf-8") as mtf:
+        mtf.write(main_tf)
+    print(f"- writing tf")
 
 if __name__ == "__main__":
     for source in upstream_sources:
         yaml_body = get(source).text
         doc = load(yaml_body, Loader=FullLoader)
         build(doc)
+        build_tf(doc)
