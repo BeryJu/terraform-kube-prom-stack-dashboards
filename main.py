834e9efb929bb8e66ffdc209cf6a4ed5b30ac00e
Jens Langhammer <jens.langhammer@beryju.org>
2022-08-18 12:55:17 +0000

    bump http version

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def build_tf(doc: dict):
     }
     http = {
       source = "hashicorp/http"
-      version = "~> 2.1"
+      version = "~> 3.0"
     }
   }
 }
--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     http = {
       source = "hashicorp/http"
-      version = "~> 2.1"
+      version = "~> 3.0"
     }
   }
 }
