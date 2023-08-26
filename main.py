cc3ba209db0015dcba72e6c829447ddcfbd4e68b
Jens Langhammer <jens.langhammer@beryju.org>
2023-08-26 19:36:05 +0200

    bump tf provider

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def build_tf(doc: dict):
   required_providers {
     grafana = {
       source = "grafana/grafana"
-      version = "~> 1.13"
+      version = "~> 2.2.0"
     }
     http = {
       source = "hashicorp/http"
